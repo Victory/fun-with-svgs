703ba2172539ce64f225d89dc731f7d37f505ba6
Victory <git@dfhu.org>
2016-02-26 17:18:03 -0500

    two circles meeting

--- orbits/orbits.js
+++ orbits/orbits.js
@@ -1,22 +1,32 @@
 var orbits = document.getElementById('orbits');
-var circle = orbits.querySelector('circle');
+var circle1 = document.getElementById('circle1');
 
 
 (function () {
   var count = 0;
   var numSteps = 80;
   var r = 200;
-  var cx = 0;
-  var cy = 0;
+  var c1x = 0; var c2x = 500;
+  var c1y = 0; var c2y = 500;
   var fps = 1000/25;
-  circle.setAttribute('cx', cx);
-  circle.setAttribute('cy', cy);
+  circle1.setAttribute('cx', c1x);
+  circle1.setAttribute('cy', c1y);
+  var circle2 = circle1.cloneNode();
+  circle2.setAttribute('id', '');
+  circle2.setAttribute('cx', c2x);
+  circle2.setAttribute('cy', c1x);
+  orbits.appendChild(circle2);  
   var roll = setInterval(function () {
-    cx += 3;
-    cy += 3;
+    c1x += 3;
+    c1y += 3;
 
-    circle.setAttribute('cx', cx);
-    circle.setAttribute('cy', cy);
+    c2x -= 3;
+    c2y -= 3;
+
+    circle1.setAttribute('cx', c1x);
+    circle1.setAttribute('cy', c1y);
+    circle2.setAttribute('cx', c2x);
+    circle2.setAttribute('cy', c2y);
     count += 1;
 
     if (count > numSteps) {
