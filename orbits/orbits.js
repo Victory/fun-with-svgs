218cb2b6724bfef65a8fc151ad126a4ed1636ede
Victory <git@dfhu.org>
2016-02-29 17:45:33 -0500

    four circles to the center

--- orbits/orbits.js
+++ orbits/orbits.js
@@ -5,35 +5,35 @@ var circle1 = document.getElementById('circle1');
 (function () {
   var numSteps = 80;
   var r = 200;
-  var c1x = 0; var c2x = 500;
-  var c1y = 0; var c2y = 500;
   var fps = 1000/25;
-  circle1.setAttribute('cx', c1x);
-  circle1.setAttribute('cy', c1y);
-  var circle2 = circle1.cloneNode();
-  circle2.setAttribute('id', '');
-  circle2.setAttribute('cx', c2x);
-  circle2.setAttribute('cy', c2y);
-  orbits.appendChild(circle2);  
+  function newCircle(circleToClone, cx, cy) {
+    var circle = circleToClone.cloneNode();
+    circle.setAttribute('id', '');
+    circle.setAttribute('cx', cx);
+    circle.setAttribute('cy', cy);
+    orbits.appendChild(circle);
+    return circle;
+  }
 
-  function* diagonal(cPosition, direction, stepSize) {
+  function* diagonal(cPosition, xDirection, yDirection, stepSize) {
     cPosition = yield cPosition;
-    var step = direction * stepSize;
+    var xStep = xDirection * stepSize;
+    var yStep = yDirection * stepSize;
     while (1) {
       var next = {
-        x: cPosition.value.x + step,
-        y: cPosition.value.y + step 
+        x: cPosition.value.x + xStep,
+        y: cPosition.value.y + yStep 
       };
       cPosition = yield next;
     }
   }
 
 
-  var moveCircle = function (circle, direction, fps) {
+  var moveCircle = function (circle, xDirection, yDirection, fps) {
     var val;
     var cx = parseInt(circle.getAttribute('cx'));
     var cy = parseInt(circle.getAttribute('cy'));
-    var kissIt = diagonal({x: cx, y: cy}, direction, 3)
+    var kissIt = diagonal({x: cx, y: cy}, xDirection, yDirection, 3)
     var last = kissIt.next(); 
     var count = 0;
     var roll = function () {
@@ -53,8 +53,14 @@ var circle1 = document.getElementById('circle1');
     var rollInterval = setInterval(roll.bind(circle), fps);
   }
 
-  moveCircle(circle1, 1, fps);
-  moveCircle(circle2, -1, fps);
+  var circle2 = newCircle(circle1, 500, 500);
+  var circle3 = newCircle(circle1, 500, 0);
+  var circle4 = newCircle(circle1, 0, 500);
+
+  moveCircle(circle1, 1, 1, fps);
+  moveCircle(circle2, -1, -1, fps);
+  moveCircle(circle3, -1, 1, fps);
+  moveCircle(circle4, 1, -1, fps);
 
 }());
 
