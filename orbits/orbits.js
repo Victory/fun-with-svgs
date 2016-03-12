d1c35cf3468501850f392dc65b98013ceea05387
Victory <git@dfhu.org>
2016-03-12 17:37:19 -0500

    cleanup

--- orbits/orbits.js
+++ orbits/orbits.js
@@ -5,7 +5,6 @@ circle1.style.display = "none";
 
 (function () {
   var numSteps = 80;
-  var r = 200;
   var fps = 1000/25;
 
   function getCircleLocation (circle) {
@@ -16,7 +15,7 @@ circle1.style.display = "none";
   }
 
   function newCircle (circleToClone, cx, cy) {
-    var circle = circleToClone.cloneNode();
+    var circle = circleToClone.cloneNode(false);
     circle.setAttribute('id', '');
     circle.setAttribute('cx', cx);
     circle.setAttribute('cy', cy);
@@ -43,7 +42,7 @@ circle1.style.display = "none";
     var val;
     var cx = parseInt(circle.getAttribute('cx'));
     var cy = parseInt(circle.getAttribute('cy'));
-    var kissIt = diagonal({x: cx, y: cy}, xDirection, yDirection, 3)
+    var kissIt = diagonal({x: cx, y: cy}, xDirection, yDirection, 3);
     var last = kissIt.next(); 
     var count = 0;
     var roll = function () {
@@ -52,16 +51,16 @@ circle1.style.display = "none";
 
       cx = val.x;
       cy = val.y;
-      this.setAttribute('cx', cx);
-      this.setAttribute('cy', cy);
+      this.setAttribute('cx', cx.toString());
+      this.setAttribute('cy', cy.toString());
 
       count += 1;
       if (count > numSteps) {
         clearInterval(rollInterval);
       }
-    }
+    };
     var rollInterval = setInterval(roll.bind(circle), fps);
-  }
+  };
 
   var orbitCircle = function (circle, radius, fps) {
     var rads = 0;
@@ -80,17 +79,19 @@ circle1.style.display = "none";
       circle.setAttribute('cx', cx);
       circle.setAttribute('cy', cy);
     }, fps);
-  }
+  };
 
   var oscillate = function (circle, start, end, fps) {
     var rads = 0;
     var step = 10;
-    var initCx = parseInt(circle.getAttribute('cx'));
-    var initCy = parseInt(circle.getAttribute('cy'));
+    var cx;
+    var cy;
+    //var initCx = parseInt(circle.getAttribute('cx'));
+    //var initCy = parseInt(circle.getAttribute('cy'));
     setInterval(function () {
       rads += .4;
       cx = parseInt(circle.getAttribute('cx'));
-      cy = parseInt(circle.getAttribute('cy')) 
+      cy = parseInt(circle.getAttribute('cy'));
       cx += step;
       cy = cy + 30 * Math.sin(rads);
 
@@ -99,13 +100,15 @@ circle1.style.display = "none";
       }
       circle.setAttribute('cx', cx);
       circle.setAttribute('cy', cy);
-    }, 80);
-  }
+    }, fps);
+  };
 
-  var spiningOrbit = function (circle, radius, oscillation, fps) {
+  var spinOrbit = function (circle, radius, oscillation, fps) {
     var orbitRads = 0;
     var oscillatingRads = 0;
     var c = getCircleLocation(circle);
+    var cx;
+    var cy;
     setInterval(function () {
       orbitRads += .1;
       oscillatingRads += .5;
@@ -116,13 +119,15 @@ circle1.style.display = "none";
       circle.setAttribute('cx', cx);
       circle.setAttribute('cy', cy);
     }, fps);
-  }
+  };
 
   var oscillatingOrbit = function (circle, radius, oscillation, fps) {
     var orbitRads = 0;
     var oscillatingRads = 0;
     var c = getCircleLocation(circle);
     var radiusModified;
+    var cx;
+    var cy;
     setInterval(function () {
       orbitRads += .1;
       oscillatingRads += .8;
@@ -137,7 +142,6 @@ circle1.style.display = "none";
 
   };
 
-  /*  
   var circle2 = newCircle(circle1, 500, 500);
   var circle3 = newCircle(circle1, 500, 0);
   var circle4 = newCircle(circle1, 0, 500);
@@ -146,10 +150,8 @@ circle1.style.display = "none";
   var circle7 = newCircle(circle1, 50, 50);
   var circle8 = newCircle(circle1, 0, 100);
   var circle9 = newCircle(circle1, 250, 250);
-  */
   var circle10 = newCircle(circle1, 250, 250);
  
-  /* 
   moveCircle(circle1, 1, 1, fps);
   moveCircle(circle2, -1, -1, fps);
   moveCircle(circle3, -1, 1, fps);
@@ -160,8 +162,7 @@ circle1.style.display = "none";
   orbitCircle(circle7, 20, fps);
   oscillate(circle8, 0, 500);
 
-  spiningOrbit(circle9, 150, 50, fps);
-  */
+  spinOrbit(circle9, 150, 50, fps);
 
   oscillatingOrbit(circle10, 200, 50, fps);
 }());
